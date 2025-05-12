a8d9ecb8099c1f11ad7e02111a3cd2962fe5aaf6
melikekatar16 <meloburda.1996@gmail.com>
2025-05-12 12:49:40 -0400

    inserts to my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,25 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
@@ -20,4 +32,38 @@ newPostBtn.addEventListener("click", function () {
 
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
+});
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  console.log("submit");
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const cardCaptionInput = addCardFormElement.querySelector(
+  "#image-caption-input"
+);
+const cardLinkInput = addCardFormElement.querySelector("#card-image-input");
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  newPostModal.classList.remove(".modal-is-opened");
+}
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+const modal = document.querySelector(".modal");
+const profileOpenBtnn = document.querySelector(".modal__is-opened");
+const profileCloseBtn = document.querySelector(".modal__close-btn");
+
+profileOpenBtn.addEventListener("click", () => {
+  modal.classList.add("modal_is-opened");
+});
+
+profileCloseBtn.addEventListener("click", () => {
+  modal.classList.remove("modal_is-opened");
 });
